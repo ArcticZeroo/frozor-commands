dede45a12c8a9f0fa6157904d9e8f9a459460321
Spencer <spencer@artix.cc>
2017-08-06 20:39:04 -0400

    Export CommandParent

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ require('./lib/polyfill');
 const {Command, CommandArg} = require('./lib/Command');
 const CommandHandler = require('./lib/CommandHandler');
 const LegacyConvert = require('./lib/LegacyConvert');
+const CommandParent = require('./lib/CommandParent');
 
-module.exports = {Command, CommandArg, CommandHandler, LegacyConvert};
+module.exports = {Command, CommandArg, CommandHandler, LegacyConvert, CommandParent};
